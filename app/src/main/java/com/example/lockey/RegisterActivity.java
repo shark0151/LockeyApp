d00099556cdc11536c292185716fec1fd6ff5eb3
Andreea Vasiliu <58026816+anndreeaagml@users.noreply.github.com>
2021-12-17 23:40:16 +0100

    Idk why it doesnt work but it doesnt

--- app/src/main/java/com/example/lockey/RegisterActivity.java
+++ app/src/main/java/com/example/lockey/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.lockey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,11 +18,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+@SuppressLint("SetTextI18n")
+
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "registeractv";
-    private User theUser;
     TextView error;
+    String _password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,20 +36,27 @@ public class RegisterActivity extends AppCompatActivity {
             startActivity(goToLogin);
         });
     }
+
     public void Register(View view) {
 
         String username = ((TextInputEditText) findViewById(R.id.registerUsername)).getText().toString();
 
         String password = ((TextInputEditText) findViewById(R.id.registerPassword)).getText().toString();
-        if (password.isEmpty() || username.isEmpty()) {
-            //Log.w(TAG, "signInWithEmail:failure", task.getException());
-            Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                    Toast.LENGTH_SHORT).show(); }
-        else {
-            verifyUsernameExistent(username);
+        String repeatPassword = ((TextInputEditText) findViewById(R.id.repeatPassword)).getText().toString();
+        _password=password;
+        if (password.isEmpty() || username.isEmpty() || repeatPassword.isEmpty()) {
+            error.setVisibility(View.VISIBLE);
+            error.setText("Please enter a username and a password");
+        } else {
+            if (!password.equals(repeatPassword)) {
+                error.setVisibility(View.VISIBLE);
+                error.setText("Passwords do not match");
+            } else
+                verifyUsernameExistent(username);
 
         }
     }
+
     private void verifyUsernameExistent(String username) {
         UserInterface usr = ApiUtils.getUserService();
         Call<User> getUserCall = usr.getUserByUsername(username);
@@ -54,10 +64,40 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
                 if (response.isSuccessful()) {
-                    theUser = response.body();
-                    error.setText("Username already existent. Please choose another username");
-                    error.setVisibility(View.VISIBLE);
-                    Log.d(TAG, theUser.toString());
+                    User userBad = response.body();
+                    if (userBad.getUsername() != null) {
+                        if (userBad.getUsername().equals(username)) {
+                            error.setText("Username already existent. Please choose another username");
+                            error.setVisibility(View.VISIBLE);
+                            Log.d(TAG, username);
+                        } else {
+                            User usr = new User();
+                            usr.setUsername(username);
+                            usr.setPassword(_password);
+                            RegisterUser(usr);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.e(TAG, t.getMessage());
+            }
+        });
+    }
+
+    private void RegisterUser(User theUser) {
+        UserInterface usr = ApiUtils.getUserService();
+        Call<User> registerUser = usr.createUser(theUser);
+        registerUser.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response.isSuccessful()) {
+                    Log.w(TAG, "Logged in");
+                    Intent gotoDevice = new Intent(RegisterActivity.this, DeviceListActivity.class);
+                    gotoDevice.putExtra("USER", theUser.getUsername());
+                    startActivity(gotoDevice);
                 } else {
                     String message = "Problem " + response.code() + " " + response.message();
                     Log.d(TAG, message);
