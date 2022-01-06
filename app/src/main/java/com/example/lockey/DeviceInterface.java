ddba74e6b8e7c78acb695943c19a7ea208a54a65
Alex F <vlad.faget@outlook.com>
2022-01-07 00:08:52 +0100

    Update DeviceInterface.java

--- app/src/main/java/com/example/lockey/DeviceInterface.java
+++ app/src/main/java/com/example/lockey/DeviceInterface.java
@@ -3,17 +3,14 @@ package com.example.lockey;
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface DeviceInterface {
-    @GET("Sensor/{id}")
-    Call<List<Device>> getAllValuesForDevice(@Path("id") String id);
+    @GET("Sensor/{DeviceID}")
+    Call<List<Device>> getAllValuesForDevice(@Path("DeviceID") String DeviceID);
 
-    @DELETE("Sensor/{id}")
-    Call<Device> deleteValuesForSensor(@Path("id") String id);
+    @DELETE("Sensor/{DeviceID}")
+    Call<Device> deleteValuesForSensor(@Path("DeviceID") String DeviceID);
 }
